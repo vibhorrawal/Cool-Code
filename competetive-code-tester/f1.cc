bdef2416bdbc4104730227d3b543637ab1e2cd9e
Vibhor Rawal <vibhorrawal@gmail.com>
2020-09-03 23:54:37 +0530

    add cp tester

---
+++ competetive-code-tester/f1.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define v1d  vector<int>
+#define v2d  vector<vector<int> >
+#define M 1e9+7
+#define T() int tc;cin>>tc;while(tc--)
+#define all(x) x.begin(),x.end()
+#define REP(i,n) for(auto i=0;i<(n);i++)
+#define FOR(i,a) for(auto i=a.begin();i!=a.end();i++)
+#define DEBUG 01
+#define pb emplace_back
+#define db(x) if (DEBUG) cout<<"[ "<<#x<<" : "<<x<<" ]"<<endl;
+#define endl '\n'
+
+void clr(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int32_t main(int argc, char const *argv[]){
+  clr();
+  int n;
+  cin>>n;
+  cout << n << endl;
+  return 0;
+}
